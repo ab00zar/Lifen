4c5d1976060547197d96abc68e24fef73d89d437
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-03-06 11:30:13 +0100

    some methods are added to worker for computations

--- level5/app/models/worker.rb
+++ level5/app/models/worker.rb
@@ -1,3 +1,29 @@
 class Worker < ApplicationRecord
     has_many :shifts
+    
+    #calculate total price (salary) of all workers
+    def self.total
+        total = 0
+        Worker.all.each do |w|
+            total += w.price
+        end
+        total
+    end
+    
+    
+    #calculate commission based on level4 rules
+    def self.compute_commission
+        @cc = total * 0.05 + compute_number_of_interim_shifts * 80
+    end
+    
+    
+    #count number of interim shifts
+    def self.compute_number_of_interim_shifts
+        number_of_interim_shifts = 0
+        Worker.all.select {|w| w.status == "interim"}.each do |interim|
+            number_of_interim_shifts += Shift.all.select {|sh| sh.worker_id == interim.id}.size
+        end
+        number_of_interim_shifts
+    end
+    
 end
