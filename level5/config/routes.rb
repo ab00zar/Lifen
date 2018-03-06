f959602788ad69670b27a0bf25237e8d91b634e5
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-03-06 11:33:40 +0100

    make workers/index as the root

--- level5/config/routes.rb
+++ level5/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :shifts
-  resources :workers
+    resources :shifts
+    resources :workers
+    root "workers#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
