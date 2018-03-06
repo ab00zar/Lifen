a7b70b374b9d859147d68077955ef57f4efe96c0
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-03-06 11:09:33 +0100

    shift model updated to calculate worker's price on create and update

--- level5/app/models/shift.rb
+++ level5/app/models/shift.rb
@@ -1,23 +1,30 @@
 class Shift < ApplicationRecord
-    belongs_to :worker
-    before_save :update_worker_price
-    
-    #update worker price (salary) while assigning a new shift to him/her
-    def update_worker_price
-        worker = Worker.find_by(id: worker_id)
-        worker.update_attributes(price: worker.price + price_calculator(worker))
+  belongs_to :worker
+  before_save :update_worker_price
+
+  #update worker price (salary) while assigning a new shift to him/her or updating a shift
+  def update_worker_price
+    if self.worker_id_changed?(from: nil)
+      self.worker.update_attributes(price: self.worker.price + price_calculator(self.worker, start_date))
+    else
+      old_worker = Worker.find_by(id: worker_id_was)
+      old_worker.update_attributes(price: old_worker.price - price_calculator(old_worker, start_date_was))
+      self.worker.update_attributes(price: self.worker.price + price_calculator(self.worker, start_date))
     end
-    
-    
-    #calculating the worker price (salary) based on his/her status
-    def price_calculator(w)
-        case w.status
-          when "medic"
-            [0,6].include?(Date.parse(start_date).wday) ? 270 * 2 : 270
-          when "interne"
-            [0,6].include?(Date.parse(start_date).wday) ? 126 * 2 : 126
-          when "interim"
-            [0,6].include?(Date.parse(start_date).wday) ? 480 * 2 : 480
-        end
+
+  end
+
+  #calculating the worker price (salary) based on his/her status
+  def price_calculator(worker, date)
+    case worker.status
+      when "medic"
+        [0,6].include?(Date.parse(date).wday) ? 270 * 2 : 270
+      when "interne"
+        [0,6].include?(Date.parse(date).wday) ? 126 * 2 : 126
+      when "interim"
+        [0,6].include?(Date.parse(date).wday) ? 480 * 2 : 480
     end
+  end
+
+  
 end
